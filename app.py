85261870bf7b957a3d7bbe38da3828c80a9c83ba
AKASH-M0 <167156237+AKASH-M0@users.noreply.github.com>
2026-05-03 13:12:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 import datetime
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('dashboard.html',
-                           status="SUCCESS",
-                           time=str(datetime.datetime.now()))
-
+    status_list = ["SUCCESS", "RUNNING", "FAILED"]
+    return render_template(
+        'dashboard.html',
+        status=random.choice(status_list),
+        time=str(datetime.datetime.now())
+    )
+    
 if __name__ == '__main__':
     app.run(port=5050)
